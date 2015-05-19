85a540d2f16c646322298828716e9231a5c194ac
root <root@ubuntuxrcloud.(none)>
2015-05-19 15:02:44 +0800

    0.0.5

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ var middleware = require('./lib/connect/middleware');
 module.exports = function(options) {
     options = extend({
         'port': 80,
-		'front_base': 'server_front',
 		'hostname': '*',//HOST
+		'front_base': 'server_front',
+		'front_hostname': 'js.t.sinajs.cn img.t.sinajs.cn',//前端的HOST
 		'back_base': 'server_back',//模拟后端的文件放置目录
 		'back_hostname': 'test.weibo.com test1.weibo.com',//后端的HOST，目的是真实模拟后端的页面路由请求，提供出前端可仿真的功能，比如 /index 对应 /html/index.html
 		'pathnamePrefix': '/', //默认为'/'
